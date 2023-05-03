6db65dfb35a8e64ab336bce8efce8b13340b5aa0
SolomonVP <Lum7023@yandex.ru>
2023-05-03 23:37:15 +0300

    refactor application.yml for connect to PosgreSQL

--- src/main/java/may/code/crazy/task/tracker/Application.java
+++ src/main/java/may/code/crazy/task/tracker/Application.java
@@ -2,12 +2,12 @@ package may.code.crazy.task.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
         SpringApplication
                 .run(Application.class, args);
-            }
+    }
 }
